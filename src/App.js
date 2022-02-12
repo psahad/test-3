f531ac767fbe508ae82067fd327b580983b54ec6
Sahad <iamsahadp@gmail.com>
2022-02-12 23:00:16 +0530

    menu height set

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import Aside from "./components/aside/Aside";
 import Main from "./components/Main";
 
+
+import { MenuStore } from "./store/Menu";
+
 function App() {
+
+  const menuState  = MenuStore.useState(s => s)
   return (
     <div className="App">
       <Aside className="App" as="Aside" />
